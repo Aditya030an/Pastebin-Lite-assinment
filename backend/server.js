f536b4fbeed32e56715c601e01818809d1bb9eec
Aditya030an <adityaanu20023@gmail.com>
2025-12-30 15:21:52 +0530

    upadte teh server page

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    try{
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () =>
-        console.log("Backend running on port " + PORT));
-    }catch(e){
-        console.log(e);
-    }
-
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("❌ Failed to connect to MongoDB:", err.message);
+  });
